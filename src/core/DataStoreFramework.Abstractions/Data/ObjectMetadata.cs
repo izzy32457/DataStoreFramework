b955dfb7a96a3362754d2dba7a90a06441649db0
Matthew McAtamney-Greenwood <matthew.greenwood@uka2.co.uk>
2021-07-28 09:34:56 +0100

    Adding initial project structure and code signing.

---
+++ src/core/DataStoreFramework.Abstractions/Data/ObjectMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using JetBrains.Annotations;
+
+namespace DataStoreFramework.Data
+{
+    [PublicAPI]
+    public class ObjectMetadata
+    {
+        [Key]
+        [Required]
+        public string Key { get; set; }
+
+        [Required]
+        [DefaultValue(MediaTypeNames.Application.Octet)]
+        public string MimeType { get; set; } = MediaTypeNames.Application.Octet;
+
+        [Required]
+        public DateTimeOffset CreatedDate { get; set; }
+
+        [Required]
+        public DateTimeOffset ModifiedDate { get; set; }
+
+        public ICollection<ObjectVersionMetadata> Versions { get; set; }
+    }
+}
---
+++ src/core/DataStoreFramework.Abstractions/Data/ObjectVersionMetadata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using System.Security.Cryptography;
+using JetBrains.Annotations;
+
+namespace DataStoreFramework.Data
+{
+    [PublicAPI]
+    public class ObjectVersionMetadata
+    {
+        [Key]
+        [Required]
+        public string Key { get; set; }
+
+        [Required]
+        [DefaultValue(MediaTypeNames.Application.Octet)]
+        public string MimeType { get; set; } = MediaTypeNames.Application.Octet;
+
+        [Required]
+        public DateTimeOffset CreatedDate { get; set; }
+
+        [Required]
+        public string VersionId { get; set; }
+
+        public int SortOrder { get; set; }
+
+        [Required]
+        public Dictionary<HashAlgorithmName, string> Hashes { get; set; }
+    }
+}
