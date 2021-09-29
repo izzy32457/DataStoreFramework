3d3f1fd62d2db39209343e8d968982fc0ea62c03
Izzy Coding <izzy32457@uka2.co.uk>
2021-09-29 17:58:44 +0100

    Adding internal classes to support better streaming of S3 objects for AwsS3 Provider implementation usage.

---
+++ src/aws/DataStoreFramework.AwsS3/AwsS3Services/S3UploadStream.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using JetBrains.Annotations;
+
+namespace DataStoreFramework.AwsS3.AwsS3Services
+{
+    /// <summary>A stream implementation that uploads data to an S3 object.</summary>
+    /// <remarks>
+    /// Code taken and adapted from: https://github.com/mlhpdx/s3-upload-stream/blob/main/S3UploadStream/S3UploadStream.cs.
+    ///
+    /// NOTE: The maximum size (at the time of writing) of a file in S3 is 5TB.
+    /// So, it isn't safe to assume all uploads will work here.
+    /// MAX_PART_SIZE times MAX_PART_COUNT is ~50TB, which is too big for S3.
+    /// </remarks>
+    [UsedImplicitly] // TODO: remove this when we actually start to use it.
+    internal class S3UploadStream : Stream
+    {
+        private const long MinPartLength = 5L * 1024 * 1024; // 5MB
+        private const long MaxPartLength = 5L * 1024 * 1024 * 1024; // 5GB max per PUT
+        private const long MaxPartCount = 10000; // no more than 10,000 parts total
+        private const long DefaultPartLength = MinPartLength;
+
+        private readonly Metadata _metadata;
+        private readonly IAmazonS3 _s3;
+
+        private bool _disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="S3UploadStream"/> class.
+        /// </summary>
+        /// <param name="s3">An Amazon S3 Client instance.</param>
+        /// <param name="s3Uri">An S3 Object Uri.</param>
+        /// <param name="partLength">The length in bytes of the uploaded object parts (Default: 5MB).</param>
+        public S3UploadStream(IAmazonS3 s3, string s3Uri, long partLength = DefaultPartLength)
+            : this(s3, new Uri(s3Uri), partLength)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="S3UploadStream"/> class.
+        /// </summary>
+        /// <param name="s3">An Amazon S3 Client instance.</param>
+        /// <param name="s3Uri">An S3 Object Uri.</param>
+        /// <param name="partLength">The length in bytes of the uploaded object parts (Default: 5MB).</param>
+        public S3UploadStream(IAmazonS3 s3, Uri s3Uri, long partLength = DefaultPartLength)
+            : this(s3, s3Uri.Host, s3Uri.LocalPath[1..], partLength)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="S3UploadStream"/> class.
+        /// </summary>
+        /// <param name="s3">An Amazon S3 Client instance.</param>
+        /// <param name="bucket">The Bucket name to upload object to.</param>
+        /// <param name="key">he object Key for the uploaded object.</param>
+        /// <param name="partLength">The length in bytes of the uploaded object parts (Default: 5MB).</param>
+        public S3UploadStream(IAmazonS3 s3, string bucket, string key, long partLength = DefaultPartLength)
+        {
+            _s3 = s3;
+            _metadata = new Metadata
+            {
+                BucketName = bucket,
+                Key = key,
+                PartLength = partLength,
+            };
+        }
+
+        /// <inheritdoc/>
+        public override bool CanRead => false;
+
+        /// <inheritdoc/>
+        public override bool CanSeek => false;
+
+        /// <inheritdoc/>
+        public override bool CanWrite => true;
+
+        /// <inheritdoc/>
+        public override long Length => _metadata.Length = Math.Max(_metadata.Length, _metadata.Position);
+
+        /// <inheritdoc/>
+        public override long Position
+        {
+            get => _metadata.Position;
+            set => throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public override void SetLength(long value)
+        {
+            _metadata.Length = Math.Max(_metadata.Length, value);
+            _metadata.PartLength = Math.Max(MinPartLength, Math.Min(MaxPartLength, _metadata.Length / MaxPartCount));
+        }
+
+        /// <inheritdoc/>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (count == 0)
+            {
+                return;
+            }
+
+            // write as much of the buffer as will fit to the current part, and if needed
+            // allocate a new part and continue writing to it (and so on).
+            var o = offset;
+            var c = Math.Min(count, buffer.Length - offset); // don't over-read the buffer, even if asked to
+            do
+            {
+                if (_metadata.CurrentStream == null || _metadata.CurrentStream.Length >= _metadata.PartLength)
+                {
+                    StartNewPart();
+                }
+
+                var remaining = _metadata.PartLength - _metadata.CurrentStream?.Length ?? 0;
+                var w = Math.Min(c, (int)remaining);
+                _metadata.CurrentStream?.Write(buffer, o, w);
+
+                _metadata.Position += w;
+                c -= w;
+                o += w;
+            }
+            while (c > 0);
+        }
+
+        /// <inheritdoc/>
+        public override void Flush() => Flush(false);
+
+        /// <inheritdoc/>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (!_disposed)
+                {
+                    Flush(true);
+                    CompleteUpload();
+                }
+            }
+
+            _disposed = true;
+            base.Dispose(disposing);
+        }
+
+        private void StartNewPart()
+        {
+            if (_metadata.CurrentStream != null)
+            {
+                Flush(false);
+            }
+
+            _metadata.CurrentStream = new MemoryStream();
+            _metadata.PartLength = Math.Min(MaxPartLength, Math.Max(_metadata.PartLength, (_metadata.PartCount / 2 + 1) * MinPartLength));
+        }
+
+        private void Flush(bool disposing)
+        {
+            if (!disposing && (_metadata.CurrentStream == null || _metadata.CurrentStream.Length < MinPartLength))
+            {
+                return;
+            }
+
+            _metadata.UploadId ??= _s3.InitiateMultipartUploadAsync(
+                    new InitiateMultipartUploadRequest
+                    {
+                        BucketName = _metadata.BucketName,
+                        Key = _metadata.Key,
+                    })
+                .GetAwaiter()
+                .GetResult()
+                .UploadId;
+
+            if (_metadata.CurrentStream == null)
+            {
+                return;
+            }
+
+            var i = ++_metadata.PartCount;
+
+            _metadata.CurrentStream.Seek(0, SeekOrigin.Begin);
+            var request = new UploadPartRequest
+            {
+                BucketName = _metadata.BucketName,
+                Key = _metadata.Key,
+                UploadId = _metadata.UploadId,
+                PartNumber = i,
+                IsLastPart = disposing,
+                InputStream = _metadata.CurrentStream,
+            };
+            _metadata.CurrentStream = null;
+
+            var upload = Task.Run(async () =>
+            {
+                var response = await _s3.UploadPartAsync(request).ConfigureAwait(false);
+                _metadata.PartETags.AddOrUpdate(i, response.ETag, (_, _) => response.ETag);
+                await request.InputStream.DisposeAsync().ConfigureAwait(false);
+            });
+            _metadata.Tasks.Add(upload);
+        }
+
+        private void CompleteUpload()
+        {
+            Task.WaitAll(_metadata.Tasks.ToArray());
+
+            if (Length > 0)
+            {
+                _s3.CompleteMultipartUploadAsync(
+                        new CompleteMultipartUploadRequest
+                        {
+                            BucketName = _metadata.BucketName,
+                            Key = _metadata.Key,
+                            PartETags = _metadata.PartETags.Select(e => new PartETag(e.Key, e.Value)).ToList(),
+                            UploadId = _metadata.UploadId,
+                        })
+                    .GetAwaiter()
+                    .GetResult();
+            }
+        }
+
+        private class Metadata
+        {
+            public string BucketName { get; init; }
+
+            public string Key { get; init; }
+
+            public long PartLength { get; set; } = DefaultPartLength;
+
+            public int PartCount { get; set; }
+
+            public string UploadId { get; set; }
+
+            public MemoryStream CurrentStream { get; set; }
+
+            public long Position { get; set; } // based on bytes written
+
+            public long Length { get; set; } // based on bytes written or SetLength, whichever is larger (no truncation)
+
+            public List<Task> Tasks { get; } = new ();
+
+            public ConcurrentDictionary<int, string> PartETags { get; } = new ();
+        }
+    }
+}
---
+++ src/aws/DataStoreFramework.AwsS3/AwsS3Services/SeekableS3Stream.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Amazon.S3;
+using Amazon.S3.Model;
+using JetBrains.Annotations;
+
+namespace DataStoreFramework.AwsS3.AwsS3Services
+{
+    /// <summary>A stream implementation that retrieves an S3 object in a seekable way.</summary>
+    /// <remarks>
+    /// Code taken and adapted from: https://github.com/mlhpdx/seekable-s3-stream/blob/master/SeekableS3Stream/SeekableS3Stream.cs.
+    /// </remarks>
+    [UsedImplicitly] // TODO: remove this when we actually start to use it.
+    internal class SeekableS3Stream : Stream
+    {
+        private const long DefaultPageLength = 25 * 1024 * 1024; // 25MB
+        private const int DefaultMaxPageCount = 20;
+
+        private readonly IAmazonS3 _s3;
+        private readonly MetaData _metadata;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SeekableS3Stream"/> class.
+        /// </summary>
+        /// <param name="s3">An Amazon S3 Client instance.</param>
+        /// <param name="bucket">The Bucket name to be accessed.</param>
+        /// <param name="key">The object Key to be accessed.</param>
+        /// <param name="page">The size for pages to be retrieved.</param>
+        /// <param name="maxPageCacheSize">The maximum number of pages to be cached.</param>
+        public SeekableS3Stream(IAmazonS3 s3, string bucket, string key, long page = DefaultPageLength, int maxPageCacheSize = DefaultMaxPageCount)
+        {
+            _s3 = s3;
+            var m = _s3.GetObjectMetadataAsync(bucket, key).GetAwaiter().GetResult();
+
+            _metadata = new MetaData
+            {
+                Bucket = bucket,
+                Key = key,
+                PageSize = page,
+                MaxPages = maxPageCacheSize,
+                Pages = new Dictionary<long, byte[]>(maxPageCacheSize),
+                HotList = new Dictionary<long, long>(maxPageCacheSize),
+                Length = m.ContentLength,
+                S3ETag = m.ETag,
+            };
+        }
+
+        /// <summary>
+        /// Gets the total number of bytes read from the S3 Object.
+        /// </summary>
+        public long TotalRead { get; private set; }
+
+        /// <summary>
+        /// Gets the total number of bytes of the S3 Object that have been downloaded.
+        /// </summary>
+        public long TotalLoaded { get; private set; }
+
+        /// <inheritdoc/>
+        public override bool CanRead => true;
+
+        /// <inheritdoc/>
+        public override bool CanSeek => true;
+
+        /// <inheritdoc/>
+        public override bool CanWrite => false;
+
+        /// <inheritdoc/>
+        public override long Length => _metadata.Length;
+
+        /// <inheritdoc/>
+        public override long Position
+        {
+            get => _metadata.Position;
+            set => Seek(value, value >= 0 ? SeekOrigin.Begin : SeekOrigin.End);
+        }
+
+        /// <inheritdoc/>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (_metadata.Position < 0 || _metadata.Position >= Length)
+            {
+                return 0;
+            }
+
+            var p = _metadata.Position;
+            do
+            {
+                var i = p / _metadata.PageSize;
+                var o = p % _metadata.PageSize;
+
+                byte[] b = null;
+                if (_metadata.Pages.ContainsKey(i))
+                {
+                    b = _metadata.Pages[i];
+                }
+
+                if (b == null)
+                {
+                    // if we have too many pages, drop the coolest
+                    while (_metadata.Pages.Count >= _metadata.MaxPages)
+                    {
+                        var trim = _metadata.Pages.OrderBy(kv => _metadata.HotList[kv.Key]).First().Key;
+                        _metadata.Pages.Remove(trim);
+                    }
+
+                    var s = i * _metadata.PageSize;
+                    var e = s + Math.Min(_metadata.PageSize, _metadata.Length - s); // read in a single page (we're looping)
+
+                    var go = new GetObjectRequest
+                    {
+                        BucketName = _metadata.Bucket,
+                        Key = _metadata.Key,
+                        EtagToMatch = _metadata.S3ETag, // ensure the object hasn't change under us
+                        ByteRange = new ByteRange(s, e),
+                    };
+
+                    _metadata.Pages[i] = b = new byte[e - s];
+                    if (!_metadata.HotList.ContainsKey(i))
+                    {
+                        _metadata.HotList[i] = 1;
+                    }
+
+                    var read = 0;
+                    using (var r = _s3.GetObjectAsync(go).GetAwaiter().GetResult())
+                    {
+                        do
+                        {
+                            read += r.ResponseStream.Read(b, read, b.Length - read);
+                        }
+                        while (read < b.Length);
+                    }
+
+                    TotalLoaded += read;
+                }
+                else
+                {
+                    _metadata.HotList[i] += 1;
+                }
+
+                var l = Math.Min(b.Length - o, count);
+                Array.Copy(b, (int)o, buffer, offset, (int)l);
+                offset += (int)l;
+                count -= (int)l;
+                p += (int)l;
+            }
+            while (count > 0 && p < _metadata.Length);
+
+            var c = p - _metadata.Position;
+            TotalRead += c;
+            _metadata.Position = p;
+            return (int)c;
+        }
+
+        /// <inheritdoc/>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            var newPosition = _metadata.Position;
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    newPosition = offset; // offset must be positive
+                    break;
+                case SeekOrigin.Current:
+                    newPosition += offset; // + or -
+                    break;
+                case SeekOrigin.End:
+                    newPosition = _metadata.Length - Math.Abs(offset); // offset must be negative?
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(origin), origin, null);
+            }
+
+            if (newPosition < 0 || newPosition > _metadata.Length)
+            {
+                throw new InvalidOperationException("Stream position is invalid.");
+            }
+
+            return _metadata.Position = newPosition;
+        }
+
+        /// <inheritdoc/>
+        public override void SetLength(long value) => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public override void Flush() => throw new NotImplementedException();
+
+        /// <summary>An internal class that keeps state for the current S3 object stream.</summary>
+        private class MetaData
+        {
+            public string Bucket { get; init; }
+
+            public string Key { get; init; }
+
+            public string S3ETag { get; init; }
+
+            public long Length { get; init; }
+
+            public long PageSize { get; init; } = DefaultPageLength;
+
+            public long MaxPages { get; init; } = DefaultMaxPageCount;
+
+            public Dictionary<long, byte[]> Pages { get; init; }
+
+            public Dictionary<long, long> HotList { get; init; }
+
+            public long Position { get; set; }
+        }
+    }
+}
